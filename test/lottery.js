7346ed327b45a1446a95d3d269d751ca6e134be2
lisun85 <70138362+lisun85@users.noreply.github.com>
2020-12-06 23:00:27 +0800

    Front-End Not Yet Developed

---
+++ test/lottery.js
@@ -0,0 +1,102 @@
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
+const expectEvent = require('@openzeppelin/test-helpers/src/expectEvent');
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
+const Lottery = artifacts.require('Lottery.sol');
+
+const balances = async addresses => {
+  const balanceResults = await Promise.all(addresses.map(address =>
+    web3.eth.getBalance(address)
+  ));
+  return balanceResults.map(balance => web3.utils.toBN(balance));
+};
+
+contract('Lottery', (accounts) => {
+  let lottery;
+  beforeEach(async () => {
+    lottery = await Lottery.new(2);
+  });
+  
+  it('Should NOT create bet if not admin', async () => {
+    await expectRevert(
+      lottery.createBet(3, 10, {from: accounts[1]}),
+      "only admin"
+    )
+  });
+
+  it('Should NOT create bet if state not idle', async () => {
+    
+    await lottery.createBet(5, 2);
+    await expectRevert(
+      lottery.createBet(3, 10),
+      "current state does not allow this"
+    )
+  });
+
+  it('Should create a bet', async () => {
+    await lottery.createBet(3, 10);
+    const betCount = await lottery.betCount();
+    const betSize = await lottery.betSize();
+    const currentState = await lottery.currentState();
+    assert(betCount.toNumber() === 3);
+    assert(betSize.toNumber() === 10);
+    assert(currentState.toNumber() === 1);
+  });
+
+  it('Should NOT bet if not in state BETTING', async () => {
+    await expectRevert(
+      lottery.bet({from: accounts[1]}),
+      "current state does not allow this"
+    );
+  });
+
+  it('Should NOT bet if not sending exact bet amount', async () => {
+    
+    await lottery.createBet(3, 10)
+    await expectRevert(
+      lottery.bet({from: accounts[1], value: 11}),
+      "can only bet exactly the bet size"
+    )
+  });
+
+  it('Should bet', async () => { //NEED TO GO OVER DO NOT UNDERSTAND
+    const players = [accounts[1], accounts[2], accounts[3]];
+    await lottery.createBet(3, web3.utils.toWei('1', 'ether'));
+
+    const balancesBefore = await balances(players); 
+    const txs = await Promise.all(players.map(player => lottery.bet({
+      value: web3.utils.toWei('1', 'ether'), 
+      from: player,
+      gasPrice: 1
+    })))
+    const balancesAfter = await balances(players); 
+    const result = players.some((_player, i) => {
+      const gasUsed = web3.utils.toBN(txs[i].receipt.gasUsed);
+      const expected = web3.utils.toBN(web3.utils.toWei('1.94', 'ether'));
+      return balancesAfter[i].sub(balancesBefore[i]).add(gasUsed).eq(expected);
+    });
+    assert(result === true);
+  });
+
+  it('Should NOT cancel if not betting', async () => {
+    await expectRevert(
+      lottery.cancel({from: accounts[0]}),
+      "current state does not allow this"
+    );
+  });
+
+  it('Should NOT cancel if not admin', async () => {
+    await lottery.createBet(3, 10, {from: accounts[0]});
+
+    await expectRevert(
+      lottery.cancel({from: accounts[1]}),
+      "only admin"
+    )
+  });
+
+  it('Should cancel', async () => {
+    await lottery.createBet(3, 10);
+    await lottery.cancel();
+    const state = await lottery.currentState();
+    assert(state.toNumber() === 0);
+  });
+});
